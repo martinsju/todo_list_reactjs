ab375b767338c04918dd6dc0e09fc8c67071c6cf
julia <agramartinsj@gmail.com>
2022-07-14 20:00:54 -0300

    created modal open and close functions

--- src/pages/listPage.js
+++ src/pages/listPage.js
@@ -1,55 +1,87 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import Modal from "../components/modal";
 
 // pre populated list
 const initialList = [
   {
     id: 1,
-    task: 'Wash dishes'
+    task: "Wash dishes",
   },
   {
     id: 2,
-    task: 'Study React'
-  }
+    task: "Study React",
+  },
 ];
 
+const listStorage = JSON.parse(localStorage.getItem("listKey"))
+console.log("listStorage: ", listStorage)
+
 const ListPage = () => {
-  let textInput = ''
-  const [list, setList] = useState(initialList)
-  const [task, setTask] = useState('')
-  
+  const textInputRef = useRef()
+  const [list, setList] = useState(listStorage || initialList)
+  const [task, setTask] = useState("")
+  const [toggleModal, setToggleModal] = useState(false)
+
   useEffect(() => {
-    textInput.focus()
+    textInputRef.current.focus();
+    localStorage.setItem("listKey", JSON.stringify(list))
+    console.log("task atual é ", list[list.length - 1])
     console.log("useEffect is running");
-  }, [list])
-  
+  }, [list]);
+
   function handleChange(event) {
-    setTask(event.target.value)
+    setTask(event.target.value);
     console.log("setTask run");
   }
-  
+
   // add task in the list
   function handleClick() {
-    let lastID = list[list.length -1].id
+    let lastID = list[list.length - 1].id;
     setList([...list, { id: lastID + 1, task: task }])
-    setTask('')
-    // EXTRA: remember to save new items in localstorage 
+    setTask("");
   }
-  
+
+  function openModal(item) {
+    setToggleModal(true)
+    console.log("item clicked ", item);
+  }
+
+  function closeModal() {
+    // conditional to not click on li item
+    if (toggleModal) {
+      setToggleModal(false);
+    }
+    console.log("ESTOU FECHANDO O MODAL");
+  }
+ 
   return (
     <div>
       <h1>This is the To-do list page</h1>
-      <div className='list'>
+      <div className="list">
         <p>Todo List:</p>
         <ul>
           {list.map((item) => (
-            <li key={item.id}>{item.id}: {item.task}</li>
+            <li key={item.id} onClick={() => openModal(item)}>
+              {item.id}: {item.task}
+            </li>
           ))}
         </ul>
       </div>
-      <input className='inputArea' type="text" placeholder="type here your next task" value={task} onChange={handleChange} ref={(input) => { textInput = input; }}></input>
+      <input
+        className="inputArea"
+        type="text"
+        placeholder="type here your next task"
+        value={task}
+        onChange={handleChange}
+        ref={textInputRef}
+      ></input>
       <button onClick={handleClick}>Add item</button>
+      <div>
+        <p>Here the modal will appear</p>
+        <Modal isVisible={toggleModal} onCloseModal={ closeModal } />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListPage
+export default ListPage;
